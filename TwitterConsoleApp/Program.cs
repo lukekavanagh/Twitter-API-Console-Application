73e8be07c6d997076867d70215d3824e4e348707
Gregory Kan <kan.gregory@gmail.com>
2015-07-27 17:08:22 +1200

    Tweets printing to console

---
+++ TwitterConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var twitter = new Twitter
+            {
+                OAuthConsumerKey = "AFYOwlijtZRkJI5AwwMsFFvl8",
+                OAuthConsumerSecret = "2aUM9J5xuDNrvNU4FoEYGcMyKtbD1MCTi4KR5TGX9g6CrNqdeY"
+            };
+
+            IEnumerable<string> tweets = twitter.GetTweets("lukekava", 10).Result;
+
+            foreach (var t in tweets)
+            {
+                Console.WriteLine(t+"\n");
+            }
+
+            Console.ReadKey();
+
+          }
+        }
+    }
+
